9dc142d65b2f599bbf46352d5bfcc7421759b7b1
Jan T. Sott <git@idleberg.com>
2026-08-23 22:44:30 +0200

    chore: bump nsis-lsp to v0.5.3

--- plugin.py
+++ plugin.py
@@ -14,7 +14,7 @@ import sublime
 import urllib.request
 import zipfile
 
-SERVER_VERSION = '0.5.2'
+SERVER_VERSION = '0.5.3'
 RELEASE_URL = 'https://github.com/idleberg/nsis-lsp/releases/download/v{version}/nsis-lsp-{version}-{target}.zip'
 USER_AGENT = 'Sublime Text LSP'
 
